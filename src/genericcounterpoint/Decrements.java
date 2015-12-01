dae3bfb12629db070feda46d197642674e37fe37
alyssa <alyssa@10.106.80.116>
2015-12-01 14:27:30 +0000

    Add Dissonant with Root decrement

--- src/genericcounterpoint/Decrements.java
+++ src/genericcounterpoint/Decrements.java
@@ -10,30 +10,31 @@ package genericcounterpoint;
  * @author alyssa
  */
 public class Decrements {
-    static int peak_trough_quota_exceed = 30;
-    static int melodic_motion_quota_exceed = 70;
-    static int successive_leaps = 50;
-    static int bad_motion_after_leap = 10;
-    static int outside_range  = 1;
-    static int remote_from_pitchcenter = 5;
-    static int illegal_note  = 100;
+    static int peak_trough_quota_exceed = 6;
+    static int melodic_motion_quota_exceed = 4;
+    static int successive_leaps = 16;
+    static int bad_motion_after_leap = 15;
+    static int outside_range  = 19;
+    static int remote_from_pitchcenter = 18;
+    static int illegal_note  = 9;
     static int overlap = 1;
-    static int accented_dissonance = 100;
-    static int bad_diss_approach_from_cons = 13;
-    static int bad_cons_approach_from_diss = 11;
-    static int motion_into_diss_both_voices_change = 12;
-    static int seq_of_diss = 1;
-    static int seq_same_type_diss = 1;
-    static int parallel_dissonance = 19;
-    static int parallel_perf_consonance = 1;
-    static int diss_cp_previous_cf = 1;
-    static int direct_motion_perf_cons = 1;
-    static int seq_of_same_cons = 1;
-    static int seq_same_type_cons = 1;
-    static int bad_diss_approach_from_diss = 1;
-    static int diss_prev_cp_next_cf_unresolved = 1;
-    static int is_not_tonic = 1;
-    static int direct_motion_into_diss = 1;
+    static int accented_dissonance = 400;
+    static int bad_diss_approach_from_cons = 21;
+    static int bad_cons_approach_from_diss = 22;
+    static int motion_into_diss_both_voices_change = 23;
+    static int seq_of_diss = 20;
+    static int seq_same_type_diss = 99;
+    static int parallel_dissonance = 98;
+    static int parallel_perf_consonance = 14;
+    static int diss_cp_previous_cf = 100;
+    static int direct_motion_perf_cons = 17;
+    static int seq_of_same_cons = 8;
+    static int seq_same_type_cons = 7;
+    static int bad_diss_approach_from_diss = 30;
+    static int diss_prev_cp_next_cf_unresolved = 5;
+    static int is_not_tonic = 2;
+    static int direct_motion_into_diss = 24;
+    static int dissonant_with_root = 2000;
     
     
     
